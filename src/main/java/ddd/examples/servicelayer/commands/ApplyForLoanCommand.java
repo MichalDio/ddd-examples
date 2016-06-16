51d243ca9a70b2e48501bf5d90fc84de17c2aa1e
Lukas Smrcka <lukas.smrcka@4finance.com>
2016-06-16 12:30:58 +0200

    Minor fix

--- src/main/java/ddd/examples/servicelayer/commands/ApplyForLoanCommand.java
+++ src/main/java/ddd/examples/servicelayer/commands/ApplyForLoanCommand.java
@@ -3,14 +3,6 @@ package ddd.examples.servicelayer.commands;
 import ddd.examples.servicelayer.base.ServiceLayerCommand;
 
 public class ApplyForLoanCommand extends ServiceLayerCommand {
-    public long getClientPersonalNumber() {
-        return clientId;
-    }
-
-    public void setClientId(long clientId) {
-        this.clientId = clientId;
-    }
-
     public long clientId;
 
     public long getClientId() {
--- src/main/java/ddd/examples/servicelayer/commands/CreateTaskForAgentrCommand.java
+++ src/main/java/ddd/examples/servicelayer/commands/CreateTaskForAgentrCommand.java
@@ -2,9 +2,6 @@ package ddd.examples.servicelayer.commands;
 
 import ddd.examples.servicelayer.base.ServiceLayerCommand;
 
-/**
- * Created by Lukas on 10.6.2016.
- */
 public class CreateTaskForAgentrCommand extends ServiceLayerCommand {
     public CreateTaskForAgentrCommand(long clientId, String whatToDo) {
     }
